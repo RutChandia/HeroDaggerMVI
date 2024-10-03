11317ad7d967d15330c7fd7f625db0421403fe69
Noemi <rutchandia@gmail.com>
2024-10-03 18:54:39 -0300

    dto, model & mapper

---
+++ app/src/main/java/cl/noemi/herodaggermvi/domain/mapper/HeroMapper.kt
@@ -0,0 +1,32 @@
+package cl.noemi.herodaggermvi.domain.mapper
+
+import cl.noemi.herodaggermvi.data.dto.HeroDTO
+import cl.noemi.herodaggermvi.domain.model.Powerstats
+import cl.noemi.herodaggermvi.domain.model.SuperHero
+
+class HeroMapper {
+    fun dtoToSuperHero(listDTO: List<HeroDTO>): List<SuperHero> {
+        return listDTO.map { heroDTO ->
+            SuperHero(
+                id = heroDTO.id ?: 1,
+                heroName = heroDTO.name ?: "",
+                fullName = heroDTO.biography?.fullName ?: "",
+                aliases = heroDTO.biography?.aliases ?: emptyList(),
+                alterEgos = heroDTO.biography?.alterEgos ?: "",
+                groupAffiliation = heroDTO.connections?.groupAffiliation ?: "",
+                alignment = heroDTO.biography?.alignment ?: "",
+                imageSmall = heroDTO.images?.sm ?: "",
+                imageMedium = heroDTO.images?.md ?: "",
+                powerStats = Powerstats(
+                    combat = heroDTO.powerstats?.combat ?: 0,
+                    durability = heroDTO.powerstats?.durability ?: 0,
+                    intelligence = heroDTO.powerstats?.intelligence ?: 0,
+                    power = heroDTO.powerstats?.power ?: 0,
+                    speed = heroDTO.powerstats?.speed ?: 0,
+                    strength = heroDTO.powerstats?.strength ?: 0
+                ),
+                occupation = heroDTO.work?.occupation ?: ""
+            )
+        }
+    }
+}
